51407459658e7eb697d6e92599b5b28c7e8b6e8c
Matsumoto Kotaro <e235737@eve.u-ryukyu.ac.jp>
2023-12-05 09:24:37 +0900

    4st commit

--- Report5/src/Main.java
+++ Report5/src/Main.java
@@ -1,17 +1,6 @@
 public class Main {
-    public class Report5 {
         public static void main(String[] args) {
-            // 条件1: String型変数 str を宣言し、初期値として null を代入する。
-            String str = null;
-    
-            // 条件2: str.length() の内容を表示しようとする。
-            try{
-                System.out.println(str.length()); // ここで NullPointerException が発生するはず
-            }catch(NullPointerException e){
-                //NullPointerExceptionが発生した場合の処理
-                System.out.println("NullPointerExceptionが発生しました。"+e.getMessage());
-            }
+            String str = "壱百満";
+            int value = Integer.parseInt(str);
         }
-    }
-    
 }
